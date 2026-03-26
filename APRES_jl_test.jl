d9e96ac81dc98f1e557df91eae3cf6531b7a8dc5
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-03-26 18:50:57 +0900

    :pencil: (Update docs)

--- APRES_jl_test.jl
+++ APRES_jl_test.jl
@@ -11,6 +11,7 @@ begin
 	using ARPES
 	using GLMakie
 	using DimensionalData
+	using Statistics
 end
 
 # ╔═╡ 7c544b98-aaf2-40cd-ba05-0caca640749a
@@ -23,120 +24,16 @@ end
 # ╔═╡ 1b009bce-f40e-4e75-a61f-1a55951540d7
 begin
 	phibin = Bins(10)
-	binned_data = groupby(data1,  phi => phibin)
-	bin_data_mean = mean.(binned_data)
+	binned_data = groupby(data2,  phi => phibin)
+	bin_data_mean = mean.(binned_data, dims=phi)
 #	heatmap(bin_data_mean)
 end
 
-# ╔═╡ 6637ddfd-80b9-412a-8907-95e63b5b21c2
-Dict(metadata(data1))
-
-# ╔═╡ 66ec088a-c407-442c-a1ca-2fc12e68ee4a
-begin
-	shape_str = "200,400,300"
-	dims_ = reverse(parse.(Int, split(shape_str, ",")))
-end
-
-
-# ╔═╡ 93631627-d146-4b97-855e-0ab7332dd507
-
-
-# ╔═╡ 27e468ad-9884-4f87-8c46-e3eb32f43401
-# ╠═╡ skip_as_script = true
-#=╠═╡
-val(data2.dims)
-  ╠═╡ =#
-
-# ╔═╡ 7a30575c-3d06-4931-a0e7-7f2fe94bb9ef
-data2.dims[dimnum(data2.dims, :phi)]
-
-# ╔═╡ d0556b3a-9dc2-4cde-8e08-ee5373013362
-parent(lookup(data2, :eV))
-
-# ╔═╡ 9e639591-b96b-4ef1-8c4b-25285a4d3530
-parent(dims(data2)[dimnum(data2,:phi)])
-
-# ╔═╡ 395d3784-943c-4343-856e-b4899cb76b55
-typeof(val(lookup(data2, :phi)))
-
-# ╔═╡ 8935e5ee-bf23-4d13-85ea-452b0dac5519
-typeof(data2.dims[1])
-
-# ╔═╡ e25e20fd-7756-43da-b2a0-40c11bd75695
-typeof(dims(data2)[1]) <: DimensionalData.Dimension
-
-# ╔═╡ 51cbdcc0-4744-47cc-af0e-6d797ca317ba
-dims(data3)[1]
-
-# ╔═╡ 071380d7-c149-4bd6-bfc3-54491064583f
-x = range(5, 9, length = 120)
-
-
-# ╔═╡ 3b05b892-a20f-4998-9cb4-76ce09d0d123
-x
-
-# ╔═╡ 4b336f59-8641-4c1b-81c1-d6e6a21a2e83
-minimum(x)
-
-# ╔═╡ b9c65489-8cf2-4c9b-9774-646fb1dc0b40
-maximum(x)
-
-# ╔═╡ ee321e81-bddc-4a5b-bf04-abba1f2bd497
-step(x)
-
-# ╔═╡ fdf01142-71e4-45a2-aedb-fdaf433f2842
-typeof(x)
-
-# ╔═╡ 90e97f0a-be06-412d-8ef9-17d561c9605c
-StepRangeLen <: StepRange 
-
-# ╔═╡ 7b7e2323-e677-4851-a150-0bcd2a8e6130
-Range <: LinRange
-
-# ╔═╡ f256c777-1bff-4d86-a446-c25053d9c86c
-LinRange <: AbstractArray
-
-# ╔═╡ 1ec47a81-25fc-4854-afc8-c45aa95b0b41
-StepRangeLen <: LinRange
-
-# ╔═╡ 2cf480b4-5751-496b-ae19-74a477527188
-LinRange <: AbstractArray
-
-# ╔═╡ d659d3da-34b8-437f-83e6-6294447f2599
-supertype(supertype(typeof(x)))
-
-# ╔═╡ 742fc928-7512-4288-b743-d2026aee2fba
-y = [1, 2, 3, 4]
-
-# ╔═╡ ee4591b6-1682-468e-b8e3-8b8d84024cd3
-y .- 1
+# ╔═╡ 6cd95a03-0170-4887-9b1b-7a0cabe89476
+bin_data_mean[2]
 
 # ╔═╡ Cell order:
 # ╠═d46591c2-1305-11f1-9828-95424f3f768a
 # ╠═7c544b98-aaf2-40cd-ba05-0caca640749a
 # ╠═1b009bce-f40e-4e75-a61f-1a55951540d7
-# ╠═6637ddfd-80b9-412a-8907-95e63b5b21c2
-# ╠═66ec088a-c407-442c-a1ca-2fc12e68ee4a
-# ╠═93631627-d146-4b97-855e-0ab7332dd507
-# ╠═27e468ad-9884-4f87-8c46-e3eb32f43401
-# ╠═7a30575c-3d06-4931-a0e7-7f2fe94bb9ef
-# ╠═d0556b3a-9dc2-4cde-8e08-ee5373013362
-# ╟─9e639591-b96b-4ef1-8c4b-25285a4d3530
-# ╠═395d3784-943c-4343-856e-b4899cb76b55
-# ╠═8935e5ee-bf23-4d13-85ea-452b0dac5519
-# ╠═e25e20fd-7756-43da-b2a0-40c11bd75695
-# ╠═51cbdcc0-4744-47cc-af0e-6d797ca317ba
-# ╠═071380d7-c149-4bd6-bfc3-54491064583f
-# ╠═3b05b892-a20f-4998-9cb4-76ce09d0d123
-# ╠═4b336f59-8641-4c1b-81c1-d6e6a21a2e83
-# ╠═b9c65489-8cf2-4c9b-9774-646fb1dc0b40
-# ╠═ee321e81-bddc-4a5b-bf04-abba1f2bd497
-# ╠═fdf01142-71e4-45a2-aedb-fdaf433f2842
-# ╠═90e97f0a-be06-412d-8ef9-17d561c9605c
-# ╠═7b7e2323-e677-4851-a150-0bcd2a8e6130
-# ╠═f256c777-1bff-4d86-a446-c25053d9c86c
-# ╠═1ec47a81-25fc-4854-afc8-c45aa95b0b41
-# ╠═2cf480b4-5751-496b-ae19-74a477527188
-# ╠═d659d3da-34b8-437f-83e6-6294447f2599
-# ╠═742fc928-7512-4288-b743-d2026aee2fba
-# ╠═ee4591b6-1682-468e-b8e3-8b8d84024cd3
+# ╠═6cd95a03-0170-4887-9b1b-7a0cabe89476
--- Tutorial.jl
+++ Tutorial.jl
@@ -11,7 +11,6 @@ begin
 	using ARPES
 	using GLMakie
 	using DimensionalData
-	using Statistics
 end
 
 # ╔═╡ 86986ec4-5186-4b11-abc6-8db4bf86cab8
@@ -62,6 +61,7 @@ Here, several important points about ARPESData are pointed out.
 
 	* :workfunction  : The workfunction of the analyser, used in the momentum conversion.
 	* :hv or :hν : The excitation photon energy.
+	* :history : a list of transformations applied to the data, for provenance tracking.
 	* :β, :χ :δ, :ξ : The angles used in momentum conversion. The definition is taken from Ref []
 """
 
@@ -123,14 +123,6 @@ band_data = k_conversion(data)
 heatmap(band_data)
 end
 
-# ╔═╡ afbf32a5-8061-47a7-a750-aa342a590481
-begin
-	phibin = Bins(10)
-	binned_data = groupby(data,  phi => phibin)
-	bin_data_mean = mean.(binned_data)
-#	heatmap(bin_data_mean)
-end
-
 # ╔═╡ 95cfa197-76f4-4a5a-a664-777cda8f98b8
 
 
@@ -138,17 +130,16 @@ end
 
 
 # ╔═╡ Cell order:
-# ╠═86986ec4-5186-4b11-abc6-8db4bf86cab8
+# ╟─86986ec4-5186-4b11-abc6-8db4bf86cab8
 # ╠═64ed254e-28bf-11f1-9c50-87021769f2be
-# ╠═c23dbd62-ec44-43aa-a865-46711ea377a2
+# ╟─c23dbd62-ec44-43aa-a865-46711ea377a2
 # ╠═f68545b1-e548-4d9e-9a19-bb1560fa2ae4
 # ╠═131c0460-4a16-4323-9f62-177dc447af5f
-# ╠═98faf1fe-06d0-4463-bd8a-1968f07b19db
+# ╟─98faf1fe-06d0-4463-bd8a-1968f07b19db
 # ╠═9a346493-7629-4fac-90f4-ec5b34738da3
-# ╠═29fab7f0-8c8d-4008-89db-3d003ec65ed8
+# ╟─29fab7f0-8c8d-4008-89db-3d003ec65ed8
 # ╠═bedb2ef2-6a42-40cd-961f-9a7070f6ed3f
-# ╠═cc3e77f5-b4f5-462e-a1a4-962697124b28
+# ╟─cc3e77f5-b4f5-462e-a1a4-962697124b28
 # ╠═631ea681-d3be-4437-81f3-dd2abb19c363
-# ╠═afbf32a5-8061-47a7-a750-aa342a590481
 # ╠═95cfa197-76f4-4a5a-a664-777cda8f98b8
 # ╠═9bad6636-d4c0-4a83-a7a8-0891d6975b4b
